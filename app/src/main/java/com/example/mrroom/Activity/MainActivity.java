4b39062d3b1733d00f2ed65f075777824d92e73c
Tanvigupta96 <guptatannu.34gmail.com>
2019-08-20 19:09:02 +0530

    adding navigation drawer

--- app/src/main/java/com/example/mrroom/Activity/MainActivity.java
+++ app/src/main/java/com/example/mrroom/Activity/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.mrroom.Activity;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,8 +21,11 @@ import com.example.mrroom.Fragment.NotificationFragment;
 import com.example.mrroom.Fragment.ProfileFragment;
 import com.example.mrroom.R;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
     ViewPager viewPager;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mtoggle;
+    private   NavigationView navigationView;
     BottomNavigationView bottomNavigationView;
     Toolbar toolbar;
 
@@ -27,98 +34,122 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView=findViewById(R.id.navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
-        toolbar=findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer);
 
-        viewPager=findViewById(R.id.viewpager);
-        setupFm(getSupportFragmentManager(),viewPager);
+
+
+        mtoggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        bottomNavigationView = findViewById(R.id.navigation);
+        navigationView=findViewById(R.id.nav_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        viewPager = findViewById(R.id.viewpager);
+        setupFm(getSupportFragmentManager(), viewPager);
         viewPager.setCurrentItem(0);
         viewPager.setOnPageChangeListener(new PageChange());
 
     }
 
-    public static void setupFm(FragmentManager fragmentManager, ViewPager viewPager){
+    public static void setupFm(FragmentManager fragmentManager, ViewPager viewPager) {
         FragmentAdapter Adapter = new FragmentAdapter(fragmentManager);
         //Add All Fragment To List
         Adapter.add(new HomeFragment(), "Home Fragment");
         Adapter.add(new MailFragment(), "Mail Fragment");
         Adapter.add(new ProfileFragment(), "Profile Fragment");
         Adapter.add(new ManagerFragment(), "Manager fragment");
-        Adapter.add(new NotificationFragment(),"Notification Fragment");
+        Adapter.add(new NotificationFragment(), "Notification Fragment");
         viewPager.setAdapter(Adapter);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+        super.onBackPressed();
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.home:
                 viewPager.setCurrentItem(0);
-
-
                 break;
             case R.id.email:
                 viewPager.setCurrentItem(1);
-
-
                 break;
             case R.id.profile:
                 viewPager.setCurrentItem(2);
-
-
                 break;
             case R.id.manager:
                 viewPager.setCurrentItem(3);
-
                 break;
             case R.id.alert:
                 viewPager.setCurrentItem(4);
-
-
                 break;
         }
+        drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
 
+    public class PageChange implements ViewPager.OnPageChangeListener {
 
-public  class PageChange implements ViewPager.OnPageChangeListener{
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
 
-    }
+        @Override
+        public void onPageSelected(int position) {
 
-    @Override
-    public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                    bottomNavigationView.setSelectedItemId(R.id.home);
+                    break;
+                case 1:
+                    bottomNavigationView.setSelectedItemId(R.id.email);
+                    break;
+                case 2:
+                    bottomNavigationView.setSelectedItemId(R.id.profile);
+                    break;
+                case 3:
+                    bottomNavigationView.setSelectedItemId(R.id.manager);
+                    break;
+                case 4:
+                    bottomNavigationView.setSelectedItemId(R.id.alert);
+                    break;
+            }
+        }
 
-        switch (position) {
-            case 0:
-                bottomNavigationView.setSelectedItemId(R.id.home);
-                break;
-            case 1:
-                bottomNavigationView.setSelectedItemId(R.id.email);
-                break;
-            case 2:
-                bottomNavigationView.setSelectedItemId(R.id.profile);
-                break;
-            case 3:
-                bottomNavigationView.setSelectedItemId(R.id.manager);
-                break;
-            case 4:
-                bottomNavigationView.setSelectedItemId(R.id.alert);
-                break;
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
         }
     }
 
     @Override
-    public void onPageScrollStateChanged(int state) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mtoggle.onOptionsItemSelected(item)) {
 
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
-}
 }
